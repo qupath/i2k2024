decbac4f02068beaea6aa414f2b4f37d0d81660b
lleplat <lleplat@ed.ac.uk>
2024-10-09 15:06:58 +0100

    basic groovy syntax script

---
+++ scripts/groovy-scripters/0-Basic Groovy syntax.groovy
@@ -0,0 +1,39 @@
+// Use 'def' to declare variables:
+def someVariable = "Some value"
+
+// Use 'println' to print some text or variable:
+println someVariable
+println "Some other value"
+
+// Semi-columns are optional
+
+// Use '[]' to create a list:
+def someList = []
+
+// Use 'add' or 'addAll' to add elements to a list:
+someList.add("someElement1")
+someList.addAll("someElement2", "someElement3")
+
+// Use 'each' to iterate over a list:
+someList.each { element -> 
+    println element
+}
+
+// Use 'findAll' to filter a list
+def elementsEndingWith1 = someList.findAll { element ->
+    element.endsWith("1")
+}
+println elementsEndingWith1    // will print [someElement1]
+
+// Use 'collect' to transform elements of a list
+def someListUpperCased = someList.collect { element ->
+    element.toUpperCase()
+}
+println someListUpperCased    // will print [SOMEELEMENT1, SOMEELEMENT2, SOMEELEMENT3]
+
+// Use the safe navigation operator '?.' instead of checking for null objects:
+if (getProject() != null && getProject().getEntry(getCurrentImageData()) != null && getProject().getEntry(getCurrentImageData()).getImageName() == "some name") {}
+// can be replaced by:
+if (getProject()?.getEntry(getCurrentImageData())?.getImageName() == "some name") {}
+
+// More information can be found on https://groovy-lang.org/​
